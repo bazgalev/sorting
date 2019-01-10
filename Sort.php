9f25e104ea0356bc34f112396077b36b45ed2885
bazgalev <pazgalevm@gmail.com>
2019-01-11 02:42:39 +0300

    Сортировка пузырьком по возрастанию

--- Sort.php
+++ Sort.php
@@ -4,6 +4,21 @@ namespace Pazgalev\Sorting\Sort {
 
     class Sort
     {
+        static function byBubble(array $array)
+        {
+            $size = count($array);
 
+            for ($i = 0; $i < $size; $i++) {
+                for ($j = $i + 1; $j < $size; $j++) {
+                    if ($array[$j] < $array[$i]) {
+                        $temp = $array[$i];
+                        $array[$i] = $array[$j];
+                        $array[$j] = $temp;
+                    }
+                }
+            }
+
+            return $array;
+        }
     }
 }
